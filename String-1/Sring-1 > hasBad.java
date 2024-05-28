c3b5cd65918358a7e3bca25b3459e2320270b895
AaravAggarwal06 <aarav.aggarwal06@gmail.com>
2024-05-28 10:18:44 +0530

    Create Sring-1 > hasBad.java

---
+++ String-1/Sring-1 > hasBad.java
@@ -0,0 +1,14 @@
+public boolean hasBad(String str) {
+ 
+
+  if(str.length()==3 && str.substring(0,3).equals("bad"))
+    return true;
+  
+  
+  if(str.length()>=4 && (str.substring(0,3).equals("bad") || str.substring(1,4).equals("bad")))
+    return true;
+  
+
+  else
+    return false;
+}
